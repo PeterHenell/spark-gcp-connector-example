f2639c4658b208266c310f32c150cac500562a24
Peter Henell <peter.henell@tele2.com>
2018-11-27 11:16:09 +0100

    removing unceccecary configuration for project id

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -13,12 +13,12 @@ object Main {
 
     import com.google.cloud.hadoop.fs.gcs.{GoogleHadoopFS, GoogleHadoopFileSystem}
 
-    spark.sparkContext.hadoopConfiguration.set("google.cloud.auth.service.account.enable", "true")
     spark.sparkContext.hadoopConfiguration.set("fs.gs.impl", classOf[GoogleHadoopFileSystem].getName)
-    spark.sparkContext.hadoopConfiguration.set("spark.hadoop.google.cloud.auth.service.account.enable", "true")
     spark.sparkContext.hadoopConfiguration.set("fs.AbstractFileSystem.gs.impl", classOf[GoogleHadoopFS].getName)
+
+    spark.sparkContext.hadoopConfiguration.set("google.cloud.auth.service.account.enable", "true")
+    spark.sparkContext.hadoopConfiguration.set("spark.hadoop.google.cloud.auth.service.account.enable", "true")
     spark.sparkContext.hadoopConfiguration.set("google.cloud.auth.service.account.json.keyfile","playground-key.json")
-    spark.sparkContext.hadoopConfiguration.set("fs.gs.project.id", "playground-193310")
 
     val csvData = spark.read.csv("gs://spark-playground/oracle-db.go")
 
